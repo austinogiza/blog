1e768d01d1eb52e7994b070c860e22d2a61f0da7
austinogiza <44683018+austinogiza@users.noreply.github.com>
2020-06-16 21:00:07 +0100

    files

--- blog/settings.py
+++ blog/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
